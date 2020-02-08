a9f163c5e80ef54a509408d05ea9dbdad0db5042
Pavithra Raghavan <pavithra.raghavanms@gmail.com>
2020-02-07 19:59:25 -0500

    wrote elapsedTime()

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,22 @@
 #include <string>
-
+#include <sstream>
 #include "format.h"
 
 using std::string;
+using std::stringstream;
 
 // TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long times) { 
+    stringstream res;
+    // add "HH:" to res
+    res << times/3600 << ":";
+    // add "MM:" to res
+    res << (times % 3600)/60 << ":";
+    // add "SS" to res
+    res << (times % 3600)%60;
+    
+    return res.str();
+}
