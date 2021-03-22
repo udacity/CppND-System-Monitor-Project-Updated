916dd64116b35b94ccebea3cdf1dadb56d2a4114
SudKul <sudhanshu.kulshrestha@gmail.com>
2021-03-22 19:18:09 +0530

    Update src/linux_parser.cpp to display Kernel version

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -178,13 +178,13 @@ string LinuxParser::OperatingSystem() {
 }
 
 string LinuxParser::Kernel() {
-  string os, kernel;
+  string os, version, kernel;
   string line;
   std::ifstream stream(kProcDirectory + kVersionFilename);
   if (stream.is_open()) {
     std::getline(stream, line);
     std::istringstream linestream(line);
-    linestream >> os >> kernel;
+    linestream >> os >> version >> kernel;
   }
   return kernel;
 }
