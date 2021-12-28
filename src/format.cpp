dc8e90f77475cbcfc6581efc9e3fe2d7362d70b9
Filipe Reis <filipe.breis@gmail.com>
2021-12-28 10:18:14 -0300

    add format functions

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,32 @@
 #include <string>
+#include <sstream>
+#include <iomanip>
+
 
 #include "format.h"
 
 using std::string;
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+
+string Format::ElapsedTime(long seconds) { 
+/* reference: https://stackoverflow.com/questions/35608291/formatting-how-to-convert-1-to-01-2-to-02-3-to-03-and-so-on */
+    
+    std::stringstream ss;
+
+    int hour = seconds/(60*60*1000);
+
+    seconds -= hour * (60*60*1000);
+
+    int minute = (seconds) / (60 * 1000);
+
+    seconds -= minute * (60 * 1000) ;
+
+    int second = (seconds ) / 1000 ;
+
+    ss << std::setw(2) << std::setfill('0') << std::to_string(hour);
+    ss << ":" << std::setw(2) << std::to_string(minute);
+    ss << ":" << std::setw(2) << std::to_string(second);
+    
+    return  ss.str();
+
+ }
