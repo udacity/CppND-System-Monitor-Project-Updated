df9a04bf8c7de7f709a0f2c4d6ee4345b75e754c
Filipe Reis <filipe.breis@gmail.com>
2021-12-28 10:12:04 -0300

    add system functions

--- include/system.h
+++ include/system.h
@@ -18,7 +18,6 @@ class System {
   std::string Kernel();               // TODO: See src/system.cpp
   std::string OperatingSystem();      // TODO: See src/system.cpp
 
-  // TODO: Define any necessary private members
  private:
   Processor cpu_ = {};
   std::vector<Process> processes_ = {};
