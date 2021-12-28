79f5f0c15fab9f702abc09995c85f195e79a6869
Filipe Reis <filipe.breis@gmail.com>
2021-12-28 10:13:55 -0300

    add processor functions

--- include/processor.h
+++ include/processor.h
@@ -7,6 +7,17 @@ class Processor {
 
   // TODO: Declare any necessary private members
  private:
+ // CPU calc aux
+  int last_user = 0;
+  int last_nice = 0;
+  int last_system = 0;
+  int last_idle = 0;
+  int last_iowait = 0;
+  int last_irq = 0;
+  int last_softirq = 0;
+  int last_steal = 0;
+  int last_guest = 0;
+  int last_gest_nice = 0;
 };
 
 #endif
--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,53 @@
 #include "processor.h"
+#include "linux_parser.h"
+#include <vector>
+#include <string>
+#include <iostream>
 
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+
+float Processor::Utilization() {
+  /* 
+  This implementation follows the proposal made by Vangelis Tasoulas
+   at https://stackoverflow.com/questions/23367857/accurate-calculation-of-cpu-usage-given-in-percentage-in-linux
+  */
+
+  std::vector<int> line_read;
+
+  line_read = LinuxParser::CpuUtilization();
+  
+  // I dont like this approach, but structured binding wouldnt work with vectors
+  int user = line_read[0];  
+  int nice = line_read[1];
+  int system = line_read[2]; 
+  int idle = line_read[3];
+  int iowait = line_read[4];
+  int irq = line_read[5];
+  int softirq = line_read[6];
+  int steal = line_read[7]; 
+  int guest = line_read[8];
+  int gest_nice = line_read[9];
+  
+  
+  int current_idle = idle + iowait;
+  int current_non_idle = user + nice + system + irq + steal;
+  int current_total = current_idle + current_non_idle;
+  
+  int last_idle_calc = last_idle + last_iowait;
+  int last_non_idle = last_user + last_nice + last_system + last_irq + last_steal; 
+  int last_total = last_idle + last_non_idle;
+
+  float cpu_use = float((current_total - last_total) - (current_idle - last_idle_calc)) / (current_total - last_total);
+
+  // store current data to be used in the future
+  last_nice = nice;
+  last_system = system;
+  last_idle = idle;
+  last_iowait = iowait;
+  last_irq = irq;
+  last_softirq = softirq;
+  last_steal = steal;
+  last_guest = guest;
+  last_gest_nice = gest_nice;
+
+  return cpu_use;
+ }
