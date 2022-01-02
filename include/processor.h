79fc688a0b135f73c668670de2004f3ea7ec133e
Filipe Reis <filipe.breis@gmail.com>
2022-01-02 12:55:02 -0300

    increase processor sizes

--- include/processor.h
+++ include/processor.h
@@ -8,16 +8,16 @@ class Processor {
   // TODO: Declare any necessary private members
  private:
   // CPU calc aux
-  long int last_user = 0;
-  long int last_nice = 0;
-  long int last_system = 0;
-  long int last_idle = 0;
-  long int last_iowait = 0;
-  long int last_irq = 0;
-  long int last_softirq = 0;
-  long int last_steal = 0;
-  long int last_guest = 0;
-  long int last_gest_nice = 0;
+  unsigned long long int last_user = 0;
+  unsigned long long int last_nice = 0;
+  unsigned long long int last_system = 0;
+  unsigned long long int last_idle = 0;
+  unsigned long long int last_iowait = 0;
+  unsigned long long int last_irq = 0;
+  unsigned long long int last_softirq = 0;
+  unsigned long long int last_steal = 0;
+  unsigned long long int last_guest = 0;
+  unsigned long long int last_gest_nice = 0;
 };
 
 #endif
--- src/processor.cpp
+++ src/processor.cpp
@@ -11,34 +11,37 @@ float Processor::Utilization() {
   https://stackoverflow.com/questions/23367857/accurate-calculation-of-cpu-usage-given-in-percentage-in-linux
   */
 
-  std::vector<long int> line_read;
+  std::vector<unsigned long long int> line_read;
 
   line_read = LinuxParser::CpuUtilization();
 
   // I dont like this approach, but structured binding wouldnt work with vectors
-  long int user = line_read[0];
-  long int nice = line_read[1];
-  long int system = line_read[2];
-  long int idle = line_read[3];
-  long int iowait = line_read[4];
-  long int irq = line_read[5];
-  long int softirq = line_read[6];
-  long int steal = line_read[7];
-  long int guest = line_read[8];
-  long int gest_nice = line_read[9];
+  unsigned long long int user = line_read[0];
+  unsigned long long int nice = line_read[1];
+  unsigned long long int system = line_read[2];
+  unsigned long long int idle = line_read[3];
+  unsigned long long int iowait = line_read[4];
+  unsigned long long int irq = line_read[5];
+  unsigned long long int softirq = line_read[6];
+  unsigned long long int steal = line_read[7];
+  unsigned long long int guest = line_read[8];
+  unsigned long long int guest_nice = line_read[9];
 
-  long int current_idle = idle + iowait;
-  long int current_non_idle = user + nice + system + irq + steal;
-  long int current_total = current_idle + current_non_idle;
+  unsigned long long int current_idle = idle + iowait;
+  unsigned long long int last_idle_calc = last_idle + last_iowait;
 
-  long int last_idle_calc = last_idle + last_iowait;
-  long int last_non_idle =
-      last_user + last_nice + last_system + last_irq + last_steal;
-  long int last_total = last_idle + last_non_idle;
+  unsigned long long int current_non_idle = user + nice + system + irq + softirq + steal;
+  unsigned long long int last_non_idle =
+      last_user + last_nice + last_system + last_irq + last_softirq + last_steal;
 
-  float cpu_use =
-      float((current_total - last_total) - (current_idle - last_idle_calc)) /
-      (current_total - last_total);
+  unsigned long long int current_total = current_idle + current_non_idle;
+  unsigned long long int last_total = last_idle_calc + last_non_idle;
+
+  unsigned long long int total_diff = current_total - last_total;
+  unsigned long long int idle_diff = current_idle - last_idle_calc;
+
+
+  double cpu_use = (total_diff - idle_diff) / float(total_diff);
 
   // store current data to be used in the future
   last_nice = nice;
@@ -49,7 +52,7 @@ float Processor::Utilization() {
   last_softirq = softirq;
   last_steal = steal;
   last_guest = guest;
-  last_gest_nice = gest_nice;
+  last_guest_nice = guest_nice;
 
   return cpu_use;
 }
